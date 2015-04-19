1f2284128388956ac9c8f57895224c8499d243c1
Joyce Echessa <jokhessa@yahoo.com>
2015-04-19 15:00:48 +0300

    Added BookDetail.js

---
+++ react_native_full_app/BookSearch/BookDetail.js
@@ -0,0 +1,21 @@
+/**
+ * Created by echessa on 4/18/15.
+ */
+
+'use strict';
+
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+class BookDetail extends React.Component {
+    render() {
+        return ();
+    }
+}
+
+module.exports = BookDetail;
