921b56faa981f2988d7961edaf2e16da456944e2
Joyce Echessa <jokhessa@yahoo.com>
2015-04-30 18:24:29 +0300

    Set Task object data on adapter

--- BF_Android/TaskIt/app/src/main/java/com/echessa/taskit/TaskListActivity.java
+++ BF_Android/TaskIt/app/src/main/java/com/echessa/taskit/TaskListActivity.java
@@ -2,16 +2,21 @@ package com.echessa.taskit;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class TaskListActivity extends ActionBarActivity {
+
+    private static final String TAG = "TaskListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,20 @@ public class TaskListActivity extends ActionBarActivity {
         items[0].setName("Task 1");
         items[1] = new Task();
         items[1].setName("Task 2");
+        items[1].setDone(true);
         items[2] = new Task();
         items[2].setName("Task 3");
 
         ListView listView = (ListView)findViewById(R.id.task_list);
         listView.setAdapter(new TaskAdapter(items));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Position clicked is " + position);
+
+            }
+        });
+
     }
 
     private class TaskAdapter extends ArrayAdapter<Task> {
@@ -38,8 +52,12 @@ public class TaskListActivity extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             convertView =  super.getView(position, convertView, parent);
+            Task task = getItem(position);
             TextView taskName = (TextView)convertView.findViewById(R.id.task_item_name);
-            taskName.setText("Hello World!");
+            taskName.setText(task.getName());
+
+            CheckBox doneBox = (CheckBox)convertView.findViewById(R.id.task_item_done);
+            doneBox.setChecked(task.isDone());
 
             return convertView;
         }
