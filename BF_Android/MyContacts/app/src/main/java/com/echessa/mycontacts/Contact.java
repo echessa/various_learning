5429d433f164a2707ba95a50d71e91c1cdfd091f
Joyce Echessa <jokhessa@yahoo.com>
2015-05-07 15:51:43 +0300

    Added ArrayAdapter subclass and a listview

---
+++ BF_Android/MyContacts/app/src/main/java/com/echessa/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.echessa.mycontacts;
+
+/**
+ * Created by Joyce Echessa on 5/7/15.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
