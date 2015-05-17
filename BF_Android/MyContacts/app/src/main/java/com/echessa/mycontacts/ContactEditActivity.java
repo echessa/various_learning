b5d4faa6a414bdcddfd45c68e8f358f0d61f0060
Joyce Echessa <jokhessa@yahoo.com>
2015-05-17 13:42:14 +0300

    Populated Edit Contact view with contact's details

--- BF_Android/MyContacts/app/src/main/java/com/echessa/mycontacts/ContactEditActivity.java
+++ BF_Android/MyContacts/app/src/main/java/com/echessa/mycontacts/ContactEditActivity.java
@@ -1,9 +1,15 @@
 package com.echessa.mycontacts;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class ContactEditActivity extends ActionBarActivity {
@@ -16,8 +22,49 @@ public class ContactEditActivity extends ActionBarActivity {
         setContentView(R.layout.activity_contact_edit);
 
         Contact contact = (Contact)getIntent().getSerializableExtra(EXTRA);
+
+        EditText editName = (EditText)findViewById(R.id.contact_edit_name);
+        editName.setText(contact.getName());
+
+        addToSection(R.id.phonenumber_section, contact.phoneNumbers);
+        addToSection(R.id.email_section, contact.emails);
+
+        TextView addNewPhoneNumber = (TextView)findViewById(R.id.add_new_phonenumber);
+        addNewPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToSection(R.id.phonenumber_section, "");
+            }
+        });
+
+        TextView addEmail = (TextView)findViewById(R.id.add_new_email);
+        addEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToSection(R.id.email_section, "");
+            }
+        });
     }
 
+    private void addToSection(int sectionID, String value) {
+        LinearLayout section = (LinearLayout)findViewById(sectionID);
+        EditText et = new EditText(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        et.setLayoutParams(lp);
+        et.setText(value);
+        section.addView(et);
+    }
+
+    private void addToSection(int sectionID, ArrayList<String> values) {
+        LinearLayout section = (LinearLayout)findViewById(sectionID);
+        for (int i = 0; i < values.size(); i++) {
+            EditText et = new EditText(this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            et.setLayoutParams(lp);
+            et.setText(values.get(i));
+            section.addView(et);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
