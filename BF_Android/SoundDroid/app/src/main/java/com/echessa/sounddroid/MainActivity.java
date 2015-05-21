edb36bbc04589f190c61e21116ee9008705c2392
Joyce Echessa <jokhessa@yahoo.com>
2015-05-21 07:31:54 +0300

    Created the SoundDroid project

---
+++ BF_Android/SoundDroid/app/src/main/java/com/echessa/sounddroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.echessa.sounddroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //id 0870cc86ff9e720019c3c139005598cb
+    //secret 5c0ce8f7881c18a70482ba18438ffb7e
+    //end user auth https://soundcloud.com/connect
+    //token https://api.soundcloud.com/oauth2/token
+    // https://api.soundcloud.com/tracks?client_id=YOUR_CLIENT_ID
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
