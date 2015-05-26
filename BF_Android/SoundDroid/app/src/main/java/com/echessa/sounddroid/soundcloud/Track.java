ae4e17ab768dd885ae0870e81bd31b8fe02a1beb
Joyce Echessa <jokhessa@yahoo.com>
2015-05-26 10:38:07 +0300

    Added field in Track object to hold the track album thumbnail

--- BF_Android/SoundDroid/app/src/main/java/com/echessa/sounddroid/soundcloud/Track.java
+++ BF_Android/SoundDroid/app/src/main/java/com/echessa/sounddroid/soundcloud/Track.java
@@ -16,27 +16,30 @@ public class Track {
     @SerializedName("id")
     private int mID;
 
+    @SerializedName("artwork_url")
+    private String mArtworkURL;
+
     public String getTitle() {
         return mTitle;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
     }
 
     public String getStreamURL() {
         return mStreamURL;
     }
 
-    public void setStreamURL(String streamURL) {
-        mStreamURL = streamURL;
-    }
-
     public int getID() {
         return mID;
     }
 
-    public void setID(int ID) {
-        mID = ID;
+    public String getArtworkURL() {
+        return mArtworkURL;
+    }
+
+    public String getAvatarURL() {
+        String avatarURL = mArtworkURL;
+        if (avatarURL != null) {
+            avatarURL.replace("large", "tiny");
+        }
+        return avatarURL;
     }
 }
