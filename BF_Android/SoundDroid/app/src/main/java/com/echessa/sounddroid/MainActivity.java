be6bd804b084cb6d97baded1bc60124d877b544b
Joyce Echessa <jokhessa@yahoo.com>
2015-05-27 10:13:37 +0300

    Added search in the menu bar

--- BF_Android/SoundDroid/app/src/main/java/com/echessa/sounddroid/MainActivity.java
+++ BF_Android/SoundDroid/app/src/main/java/com/echessa/sounddroid/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.search_view) {
             return true;
         }
 
