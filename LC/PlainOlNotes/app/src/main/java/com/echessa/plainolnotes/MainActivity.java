636779a5645b16c949888a752856675221c3ecde
Joyce Echessa <jokhessa@yahoo.com>
2015-06-20 17:04:08 +0300

    Set up the database

--- LC/PlainOlNotes/app/src/main/java/com/echessa/plainolnotes/MainActivity.java
+++ LC/PlainOlNotes/app/src/main/java/com/echessa/plainolnotes/MainActivity.java
@@ -1,17 +1,30 @@
 package com.echessa.plainolnotes;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote("New note");
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
 
