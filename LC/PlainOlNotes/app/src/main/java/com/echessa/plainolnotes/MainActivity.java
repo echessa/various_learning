37a2475d57e26d6cfc44806786dfd150ea7d763d
Joyce Echessa <jokhessa@yahoo.com>
2015-06-21 06:46:41 +0300

    Customized list view

--- LC/PlainOlNotes/app/src/main/java/com/echessa/plainolnotes/MainActivity.java
+++ LC/PlainOlNotes/app/src/main/java/com/echessa/plainolnotes/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
 
         String[] from = {DBOpenHelper.NOTE_TEXT};
-        int[] to = {android.R.id.text1};
-        cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, from, to, 0);
+        int[] to = {R.id.tvNote};
+        cursorAdapter = new SimpleCursorAdapter(this, R.layout.note_list_item, null, from, to, 0);
 
         ListView list = (ListView)findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
