4e97159a077f9b152f1e2d5dbaa2a9f0cf8cba2b
Joyce Echessa <jokhessa@yahoo.com>
2015-09-09 00:57:52 +0300

    BNR ch07

---
+++ BNR_Android/Ed_02/CriminalIntent/app/src/main/java/com/echessa/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.echessa.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by echessa on 9/9/15.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+}
