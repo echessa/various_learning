ddca53972e49a731636c55c787d97a41e05c6fc6
Joyce Echessa <jokhessa@yahoo.com>
2015-09-18 13:43:07 +0300

    BNR ch13

--- BNR_Android/Ed_02/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeLab.java
+++ BNR_Android/Ed_02/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeLab.java
@@ -23,12 +23,6 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
     public List<Crime> getCrimes() {
@@ -42,6 +36,10 @@ public class CrimeLab {
             }
         }
         return null;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
 }
--- BNR_Android/Ed_02/CriminalIntent/app/src/main/java/com/echessa/criminalintent/SingleFragmentActivity.java
+++ BNR_Android/Ed_02/CriminalIntent/app/src/main/java/com/echessa/criminalintent/SingleFragmentActivity.java
@@ -2,13 +2,13 @@ package com.echessa.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by echessa on 9/11/15.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
