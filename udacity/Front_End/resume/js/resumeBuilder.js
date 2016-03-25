4b22f1ebca8db695f39b7be7fa939f3a9d2e6750
Joyce Echessa <jokhessa@yahoo.com>
2016-03-25 13:39:51 +0300

    PS Bootstrap 3 midway

--- udacity/Front_End/resume/js/resumeBuilder.js
+++ udacity/Front_End/resume/js/resumeBuilder.js
@@ -1,3 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name = "Joyce Echessa";
+ var role = "Full Stack JavaScript Developer";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+ var bio = {
+   "name": name,
+   "role": role,
+   "contact info": {
+     "mobile": "123",
+     "email": "jo@jo.com",
+     "location": "Diagon Alley"
+   },
+   "picture URL": "",
+   "welcome message": "yo",
+   "skills": []
+ }
