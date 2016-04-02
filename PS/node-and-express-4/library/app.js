ce4412a758bae8b6606c31834d5bb02a16c9aed8
Joyce Echessa <jokhessa@yahoo.com>
2016-04-02 07:28:56 +0300

    Add dependencies and Gulp task

--- PS/node-and-express-4/library/app.js
+++ PS/node-and-express-4/library/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
 app.listen(port, function(err) {
     console.log('running server on port ' + port);
-})
+});
