7e0d5d7296734bae3d2cb65724b413a417c25172
Joyce Echessa <jokhessa@yahoo.com>
2016-04-02 13:24:40 +0300

    Modify 'inject' Gulp task

--- PS/node-and-express-4/library/gulpfile.js
+++ PS/node-and-express-4/library/gulpfile.js
@@ -16,11 +16,12 @@ gulp.task('style', function() {
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var options = {
-        bowerJson: require('.bower.json'),
-        directory: '.public/lib'
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
     }
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
-        pipe(gulp.dest('./src/views'));
+        .pipe(gulp.dest('./src/views'));
 });
