061e815531e078fde6cf13a3c9a4caf68242adf8
Joyce Echessa <jokhessa@yahoo.com>
2016-05-28 12:40:18 +0300

    CH02

---
+++ nodejs/web_dev_with_node_book/CH_02/helloWorld.js
@@ -0,0 +1,39 @@
+var http = require('http'),
+    fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) {
+        responseCode = 200;
+    }
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type' : 'text/plain' });
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode, { 'Content-Type' : contentType });
+            res.end(data);
+        }
+    })
+}
+
+http.createServer(function(req, res) {
+    // normalize url by removing querystring, optional
+    // trailing slash, and making lowercase
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+switch (path) {
+    case '':
+        serveStaticFile(res, '/public/home.html', 'text/html');
+        break;
+    case '/about':
+        serveStaticFile(res, '/public/about.html', 'text/html');
+        break;
+    case '/img/logo.png':
+        serveStaticFile(res, '/public/img/logo.png', 'image/png');
+        break;
+    default:
+        serveStaticFile(res, '/public/404.html', 'text/html', 404);
+        break;
+}
+}).listen(3000);
+
+console.log('Server started on localhost:3000');
