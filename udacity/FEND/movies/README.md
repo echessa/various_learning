dac62c241462d685e19b74f015b4e4e3e9a5bf90
Joyce Echessa <jokhessa@yahoo.com>
2016-06-25 15:21:13 +0300

    Modify files

---
+++ udacity/FEND/movies/README.md
@@ -0,0 +1,9 @@
+# Movie Trailer Website: Fresh Tomatoes
+
+## About
+
+This project provides a simple server-side script that stores a list of movies and generates a static web page that allows visitors to browse the movies and watch their trailers.
+
+## Getting Started
+
+To run the program, change the Terminal's directory to the root of the project's folder and run `python entertainment_center.py`. A file named **fresh_tomatoes.html** will be generated and opened in your default web browser.
--- udacity/FEND/movies/media.py
+++ udacity/FEND/movies/media.py
@@ -1,16 +1,15 @@
 import webbrowser
 
 class Movie():
-    """This class provides a way to store movie related information"""
+    """This class stores movie related information.
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    Attributes:
+        title: A String for the title of the movie
+        poster_image_url: A String for the URL of the movie's box art image
+        trailer_youtube_url: A String for the URL of the movie's trailer on YouTube
+    """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-# title, duration
