7817b20816289864c346cf8f4376a65faed69070
Joyce Echessa <jokhessa@yahoo.com>
2016-09-12 06:17:30 +0300

    Messenger bot test

---
+++ PS/EventReg/app/js/controllers/EditEventController.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var eventsApp = angular.module("eventsApp");
+
+    var EditEventController = function($scope) {
+
+    };
+
+    eventsApp.controller('EditEventController', EditEventController);
+
+}());
