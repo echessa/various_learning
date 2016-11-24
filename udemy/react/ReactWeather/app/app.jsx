65775a64cef16a8ccc8469dfbe04f95003f735d6
Joyce Echessa <jokhessa@yahoo.com>
2016-11-24 17:26:16 +0300

    Switch to scss

--- udemy/react/ReactWeather/app/app.jsx
+++ udemy/react/ReactWeather/app/app.jsx
@@ -11,7 +11,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
