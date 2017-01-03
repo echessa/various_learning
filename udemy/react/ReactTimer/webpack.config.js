c4cbf9577ed210bfd5f3b99a74b42ef1ebae09a9
Joyce Echessa <jokhessa@yahoo.com>
2017-01-03 12:53:26 +0300

    Add Controls component and test

--- udemy/react/ReactTimer/webpack.config.js
+++ udemy/react/ReactTimer/webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
             Clock: 'app/components/Clock.jsx',
-            CountdownForm: 'app/components/CountdownForm.jsx'
+            CountdownForm: 'app/components/CountdownForm.jsx',
+            Controls: 'app/components/Controls.jsx'
         },
         extensions: ['', '.js', '.jsx']
     },
