5635f3c7c8802d5c38ac3bf9fb18e691e2ecbc93
Joyce Echessa <jokhessa@yahoo.com>
2017-01-10 13:12:23 +0300

    Add more tests

--- udemy/react/ReactTimer/app/components/Countdown.jsx
+++ udemy/react/ReactTimer/app/components/Countdown.jsx
@@ -62,6 +62,7 @@ var Countdown = React.createClass({
 
         return (
             <div>
+                <h1 className="page-title">Countdown App</h1>
                 <Clock totalSeconds={count}/>
                 {renderControlArea()}
             </div>
--- udemy/react/ReactTimer/app/tests/components/Countdown.test.jsx
+++ udemy/react/ReactTimer/app/tests/components/Countdown.test.jsx
@@ -34,5 +34,29 @@ describe('Countdown', () => {
                 done();
             }, 3001)
         });
+
+        it('should pause countdown on paused status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should reset count on stopped', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+                done();
+            }, 1001);
+        });
     });
 });
