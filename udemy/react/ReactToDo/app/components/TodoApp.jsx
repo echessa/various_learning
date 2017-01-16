33363d9b86b9596144ca84d536b9c8f25cb311ac
Joyce Echessa <jokhessa@yahoo.com>
2017-01-16 12:20:42 +0300

    Add TodoApp test

--- udemy/react/ReactToDo/app/components/TodoApp.jsx
+++ udemy/react/ReactToDo/app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
     getInitialState: function() {
@@ -10,26 +11,34 @@ var TodoApp = React.createClass({
             searchText: '',
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: 'Walk the dog'
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: 'Clean the yard'
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: 'Study React'
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: 'Reply to email'
                 }
             ]
         };
     },
     handleAddTodo: function (text) {
-        alert('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        });
     },
     handleSearch: function (showCompleted, searchText) {
         this.setState({
