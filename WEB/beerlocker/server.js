f993b091d42d8c3bd5e215ecdca8b96a01956837
Joyce Echessa <jokhessa@yahoo.com>
2017-03-31 08:30:56 +0300

    Add User model

--- WEB/beerlocker/server.js
+++ WEB/beerlocker/server.js
@@ -1,8 +1,9 @@
-// Get the packages we need
+// Load required packages
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
 
 // Connect to the beerlocker MongoDB
 mongoose.connect('mongodb://localhost:27017/beerlocker');
@@ -21,82 +22,24 @@ var port = process.env.PORT || 3000;
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
-});
+// Create endpoint handlers for /beers
+router.route('/beers')
+  .post(beerController.postBeers)
+  .get(beerController.getBeers);
 
-// Create a new route with the prefix /beers
-var beersRoute = router.route('/beers');
+// Create endpoint handlers for /beers/:beer_id
+router.route('/beers/:beer_id')
+  .get(beerController.getBeer)
+  .put(beerController.putBeer)
+  .delete(beerController.deleteBeer);
 
-// Create endpoint /api/beers for POSTS
-beersRoute.post(function(req, res) {
-  // Create a new instance of the Beer model
-  var beer = new Beer();
-
-  // Set the beer properties that came from the POST data
-  beer.name = req.body.name;
-  beer.type = req.body.type;
-  beer.quantity = req.body.quantity;
-
-  // Save the beer and check for errors
-  beer.save(function(err) {
-    if (err)
-      res.send(err);
-
-    res.json({ message: 'Beer added to the locker!', data: beer });
-  });
-});
-
-// Create endpoint /api/beers for GET
-beersRoute.get(function(req, res) {
-  // Use the Beer model to find all beer
-  Beer.find(function(err, beers) {
-    if (err)
-      res.send(err);
-
-    res.json(beers);
-  });
-});
-
-// Create a new route with the /beers/:beer_id prefix
-var beerRoute = router.route('/beers/:beer_id');
-
-// Create endpoint /api/beers/:beer_id for GET
-beerRoute.get(function(req, res) {
-  // Use the Beer model to find a specific beer
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err)
-      res.send(err);
-
-    res.json(beer);
-  });
-});
-
-// Create endpoint /api/beers/:beer_id for PUT
-beerRoute.put(function(req, res) {
-  // Use the Beer model to find a specific beer
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err)
-      res.send(err);
-
-    // Update the existing beer quantity
-    beer.quantity = req.body.quantity;
-
-    // Save the beer and check for errors
-    beer.save(function(err) {
-      if (err)
-        res.send(err);
-
-      res.json(beer);
-    });
-  });
-});
+// Create endpoint handlers for /users
+router.route('/users')
+  .post(userController.postUsers)
+  .get(userController.getUsers);
 
 // Register all our routes with /api
 app.use('/api', router);
 
 // Start the server
 app.listen(port);
-console.log('Insert beer on port ' + port);
